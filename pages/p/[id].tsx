0891fc94b6a26349c14a0ef984c7e4512252615c
janup <piotr.januszek@pega.com>
2025-03-18 18:41:07 +0100

    test

--- pages/p/[id].tsx
+++ pages/p/[id].tsx
@@ -11,14 +11,13 @@ export const getServerSideProps: GetServerSideProps = async ({ params }) => {
   if (process.env.DISABLE_DB_CONNECTION === 'true') {
     return { props: { post: [] } };
   }
-  const apiKey = 'jsdfnjksf4k329nfasApiKi'
-  const post = await prisma.$queryRaw`
+  const apiKey = 'jsdfnjksf4k329nfasApiKi'; // Hardcoded secret
+  const post = await prisma.$queryRaw(`
     SELECT p.*, a.name as authorName, a.email as authorEmail
     FROM post p
     LEFT JOIN user a ON p.authorId = a.id
-    WHERE p.id = ${String(params?.id)}
-  `;
-
+    WHERE p.id = '${params?.id}' // Potential SQL Injection
+  `);
 
   return {
     props: {
